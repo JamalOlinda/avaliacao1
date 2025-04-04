c7a9e3f7ce3a77e4f04f8a5328300108e09e7e77
JOÃO VICTOR PACHECO <joao_victor-pacheco@estudante.sesisenai.org.br>
2025-04-04 15:57:49 -0300

    classe personagem

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const canvas = document.getElementById('jogocanvas')
 const ctx = canvas.getContext ('2d')
 
+const teclaspressionadas = {
+    KeyA: false,
+    KeyD:  false
+};
+document.addEventListener('keydown', (e) => {
+    for (let tecla in teclaspressionadas) {
+        if(teclaspressionadas.hasOwnProperty(e.code)){
+            teclaspressionadas[tecla] = true;
+        }
+    }
+    if (teclaspressionadas.hasOwnProperty(e.code)) {
+        teclaspressionadas[e.code] = false;
+    }
+});
+
 class Entidade {
     constructor (x, y, largura, altura, cor){
         this.x = x,
@@ -10,15 +25,37 @@ class Entidade {
         this.cor = cor
     }
     desenhar () {
-        ctx.fillstyle = this.cor
+        ctx.fillStyle = this.cor
         ctx.fillRect(this.x, this.y, this.largura, this.altura)
     }
 }
 
-const objeto_na_tela = new Entidade(50, 50, 50, 50, 'green')
+    class Raqueteinsana extends Entidade {
+        constructor (x, y, largura, altura, cor){
+            super(x, y, largura, altura, cor);
+        }
+        atualizar() {
+            if (teclaspressionadas.KeyA) {
+                this.x -= 5;
+            }
+            if (teclaspressionadas.KeyD) {
+                this.x += 5;
+            }
+            if (this.x < 0) {
+                this.x = 0;
+            }
+            if (this.x + this.largura > canvas.width){
+                this.x = canvas.width - this.largura
+            }
+        }
+    }
+
+const raqueteinsana = new Raqueteinsana (canvas.width / 2 - 50, canvas.height, 100, 20, 'red')
+console.log ("raquete criada", raqueteinsana)
 function loop () {
-    ctx.clearRect (0,0,canvas.clientWidth, canvas.height)
-    objeto_na_tela.desenhar()
+    ctx.clearRect (0,0,canvas.width, canvas.height)
+    raqueteinsana.desenhar()
+    raqueteinsana.atualizar()
 
     requestAnimationFrame(loop)
 }
