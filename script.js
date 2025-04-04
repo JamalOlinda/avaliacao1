0dcb2ee9912fceb456a9e9176473b85da93d1aef
JOÃO VICTOR PACHECO <joao_victor-pacheco@estudante.sesisenai.org.br>
2025-04-04 16:16:40 -0300

    criei bola

--- script.js
+++ script.js
@@ -50,12 +50,49 @@ class Entidade {
         }
     }
 
-const raqueteinsana = new Raqueteinsana (canvas.width / 2 - 50, canvas.height, 100, 20, 'red')
+    class Bolarebimbante {
+        constructor(x, y, raio, cor, dx, dy){
+            this.x = x
+            this.y = y
+            this.raio = raio
+            this.cor = cor
+            this.dx = dx
+            this.dy = dy
+        }
+        desenhar() {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.raio, 0, Math.PI * 2)
+            ctx.fillStyle = this.cor
+            ctx.fill();
+            ctx.closePath();
+        }
+        atualizar (raqueteinsana){
+            this.x += this.dx;
+            this.y += this.dy;
+
+            if (this.x + this.raio > canvas.width || this.x - this.raio < 0){
+                this.dx *= -1;
+            }
+            if (this.y - this.raio < 0){
+                this.dy *= -1;
+            }
+            if (this.y + this.raio > raqueteinsana.y && this.x > raqueteinsana.x && this.x < raqueteinsana.x + raqueteinsana.largura) {
+                this.dy *= -1;
+                this.y = raqueteinsana.y - this.raio
+            }
+        }
+    }
+
+const raqueteinsana = new Raqueteinsana (canvas.width / 2 - 50, canvas.height - 30, 100, 20, 'red')
+const bola = new Bolarebimbante (canvas.width / 2, canvas.height / 2, 8, 'white', 4, -4)
 console.log ("raquete criada", raqueteinsana)
 function loop () {
     ctx.clearRect (0,0,canvas.width, canvas.height)
-    raqueteinsana.desenhar()
-    raqueteinsana.atualizar()
+    raqueteinsana.desenhar();
+    raqueteinsana.atualizar();
+    bola.desenhar();
+    bola.atualizar(raqueteinsana);
 
     requestAnimationFrame(loop)
 }
+loop()
